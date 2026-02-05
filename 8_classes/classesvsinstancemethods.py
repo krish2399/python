885cbb9140699f98dc9df5b817a644be5654becb
krish2399 <krish.nivzen@gmail.com>
2026-02-05 12:50:17 +0530

    commit

--- 8_classes/classesvsinstancemethods.py
+++ 8_classes/classesvsinstancemethods.py
@@ -1,15 +1,51 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     @classmethod
+#     def zero(cls): # for the pass value
+#         return cls(0, 0)
+
+#     def draw(self):   #for print
+#         print(f"Point {self.x},{self.y}")
+
+
+# point = Point.zero()
+# point.draw()
+
+
+# class MyName:
+#     def __init__(self, firstname, lastname):
+#         self.firstname = firstname
+#         self.lastname = lastname
+
+#     @classmethod
+#     def value(cls):
+#         return cls("Krish", "gujarati")
+
+#     def pri(self):
+#         print(f"{self.firstname} {self.lastname}")
+
+
+# name = MyName.value()
+# name.pri()
+
+
+class MyCars:
+    def __init__(self, firstcar, secondcar, thirdcar):
+        self.firstcar = firstcar
+        self.secondcar = secondcar
+        self.thirdcar = thirdcar
 
     @classmethod
-    def zero(cls):
-        return cls(0, 0)
+    def valuecar(cls):
+        return cls("Scoripo Classic S11", "BMW M5 Compititon ", "Defender Octa")
 
-    def draw(self):
-        print(f"Point{self.x},{self.y}")
+    def showcar(self):
+        print(
+            f"My first car is {self.firstcar}\nMy Second car is {self.secondcar} \nMy Third car is {self.thirdcar} ")
 
 
-point = Point.zero()
-point.draw()
+allcars = MyCars.valuecar()
+allcars.showcar()
